e0abb6d229ceea0dbb3fb6f2ff66ba5b28387bc2
Ramruthwick Pathireddy <rpathire@caltech.edu>
2015-04-15 10:14:25 -0700

    Added several tests for methods for Query_Evaluator class.

--- test_query_evaluator.py
+++ test_query_evaluator.py
@@ -4,9 +4,9 @@ from query_evaluator import Query_Evaluator
 class TestQueryEvaluator(unittest.TestCase):
 
     def test_add_node(self):
-        ''' 
+        """ 
         Tests add_node method of Query_Evaluator.
-        '''
+        """
         q = Query_Evaluator()
         attrs1 = {'Label' : 'Person', 'Name' : 'You'}
         attrs2 = {'Label' : 'Person', 'Name' : 'Me'}
@@ -25,9 +25,9 @@ class TestQueryEvaluator(unittest.TestCase):
         self.assertEqual(node3[1], attrs3)
 
     def test_add_relationship(self):
-        '''
+        """
         Tests add_relationship method for Query_Evaluator.
-        '''
+        """
         q = Query_Evaluator()
         attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
         attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
@@ -43,6 +43,155 @@ class TestQueryEvaluator(unittest.TestCase):
         # Test second edge
         edge2 = q.add_relationship(node2, node3, edge_attrs2)
         self.assertEqual(edge2, (node2[0], node3[0], edge_attrs2))
+
+    def test_match_node(self):
+        """
+        Tests match_node method for Query_Evaluator.
+        """
+        q = Query_Evaluator()
+        attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
+        attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
+        attrs3 = {'Label' : 'Person', 'Name' : 'John'}
+        node1 = q.add_node(attrs1)
+        node2 = q.add_node(attrs2)
+        node3 = q.add_node(attrs3)
+        # Test matching all node
+        match_lst1 = q.match_node({'Label' : 'Person'})
+        self.assertEqual(match_lst1, [node1, node2, node3])
+        # Test matching single node
+        match_lst2 = q.match_node({'Name' : 'Alice'})
+        self.assertEqual(match_lst2, [node1])
+        
+    def test_match_rel(self):
+        """
+        Tests match_rel method for Query_Evaluator.
+        """
+        q = Query_Evaluator()
+        attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
+        attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
+        attrs3 = {'Label' : 'Person', 'Name' : 'John'}
+        node1 = q.add_node(attrs1)
+        node2 = q.add_node(attrs2)
+        node3 = q.add_node(attrs3)
+        edge_attrs1 = {'rel_type' : 'friend'}
+        edge_attrs2 = {'rel_type' : 'friend', 'fam' : 'cousin'}
+        edge1 = q.add_relationship(node1, node2, edge_attrs1)
+        edge2 = q.add_relationship(node2, node3, edge_attrs2)
+        # Test matching relationship with multiple results
+        match_lst1 = q.match_rel(edge_attrs1)
+        result1 = [(node1[0], node2[0], edge_attrs1), 
+                    (node2[0], node3[0], edge_attrs2)]
+        self.assertEqual(sorted(match_lst1), sorted(result1))
+        # Test matching relationship with single results
+        match_lst2 = q.match_rel(edge_attrs2)
+        result2 = [(node2[0], node3[0], edge_attrs2)]
+        self.assertEqual(match_lst2, result2)
+
+    def test_match_node_rel(self):
+        """
+        Tests match_node_rel method for Query_Evaluator.
+        """
+        q = Query_Evaluator()
+        attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
+        attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
+        attrs3 = {'Label' : 'Person', 'Name' : 'John'}
+        node1 = q.add_node(attrs1)
+        node2 = q.add_node(attrs2)
+        node3 = q.add_node(attrs3)
+        edge_attrs1 = {'rel_type' : 'friend'}
+        edge_attrs2 = {'rel_type' : 'friend', 'fam' : 'cousin'}
+        edge1 = q.add_relationship(node1, node2, edge_attrs1)
+        edge2 = q.add_relationship(node2, node3, edge_attrs2)
+        # Test matching with no results
+        match_lst1 = q.match_node_rel(node1[1], edge_attrs2)
+        result1 = []
+        self.assertEqual(match_lst1, result1)
+        # Test matching with single result
+        match_lst2 = q.match_node_rel(node2[1], edge_attrs2)
+        result2 = [(node2[0], node3[0], edge_attrs2)]
+        self.assertEqual(match_lst2, result2)
+        # Test matching with multiple result
+        match_lst3 = q.match_node_rel(node2[1], {'rel_type' : 'friend'})
+        result3 = [(node1[0], node2[0], edge_attrs1),
+                    (node2[0], node3[0], edge_attrs2)]
+        self.assertEqual(match_lst3, result3)
+
+    def test_match_find_rel(self):
+        """
+        Tests match_find_rel method for Query_Evaluator.
+        """
+        q = Query_Evaluator()
+        attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
+        attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
+        attrs3 = {'Label' : 'Person', 'Name' : 'John'}
+        node1 = q.add_node(attrs1)
+        node2 = q.add_node(attrs2)
+        node3 = q.add_node(attrs3)
+        edge_attrs1 = {'rel_type' : 'friend'}
+        edge_attrs2 = {'rel_type' : 'friend', 'fam' : 'cousin'}
+        edge1 = q.add_relationship(node1, node2, edge_attrs1)
+        edge2 = q.add_relationship(node2, node3, edge_attrs2)
+        # Test matching with no results
+        match_lst1 = q.match_find_rel(node1[1], node3[1])
+        result1 = []
+        self.assertEqual(match_lst1, result1)
+        # Test matching with result
+        match_lst2 = q.match_find_rel(node1[1], node2[1])
+        result2 = [(node1[0], node2[0], edge_attrs1)]
+        self.assertEqual(match_lst2, result2)
+
+    def test_match_node_node_rel(self):
+        """
+        Tests match_node_node_rel method for Query_Evaluator.
+        """
+        q = Query_Evaluator()
+        attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
+        attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
+        attrs3 = {'Label' : 'Person', 'Name' : 'John'}
+        node1 = q.add_node(attrs1)
+        node2 = q.add_node(attrs2)
+        node3 = q.add_node(attrs3)
+        edge_attrs1 = {'rel_type' : 'friend'}
+        edge_attrs2 = {'rel_type' : 'friend', 'fam' : 'cousin'}
+        edge1 = q.add_relationship(node1, node2, edge_attrs1)
+        edge2 = q.add_relationship(node2, node3, edge_attrs2)
+        # Test matching with no results
+        match_lst1 = q.match_node_node_rel(node1[1], node2[1], edge_attrs2)
+        result1 = []
+        self.assertEqual(match_lst1, result1)
+        # Test matching with single result
+        match_lst2 = q.match_node_node_rel(node1[1], node2[1], edge_attrs1)
+        result2 = [(node1[0], node2[0], edge_attrs1)]
+        self.assertEqual(match_lst2, result2)
+        # Test matching with multiple results
+        match_lst3 = q.match_node_node_rel({'Label' : 'Person'},
+                        {'Label' : 'Person'}, {'rel_type' : 'friend'})
+        result3 = [(node1[0], node2[0], edge_attrs1),
+                    (node2[0], node3[0], edge_attrs2)]
+        self.assertEqual(match_lst3, result3)
+
+
+    def test_delete_node(self):
+        """
+        Tests delete_node method for Query_Evaluator.
+        """
+        q = Query_Evaluator()
+        attrs1 = {'Label' : 'Person', 'Name' : 'Alice'}
+        attrs2 = {'Label' : 'Person', 'Name' : 'Bob'}
+        attrs3 = {'Label' : 'Person', 'Name' : 'John'}
+        node1 = q.add_node(attrs1)
+        node2 = q.add_node(attrs2)
+        node3 = q.add_node(attrs3)
+        edge_attrs1 = {'rel_type' : 'friend'}
+        edge_attrs2 = {'rel_type' : 'friend', 'fam' : 'cousin'}
+        edge1 = q.add_relationship(node1, node2, edge_attrs1)
+        edge2 = q.add_relationship(node2, node3, edge_attrs2)
+        # Tests deleting no nodes
+        q.delete_node({'Label' : 'Person', 'Name' : 'Fred'})
+        match_lst1 = q.match({}, {}, {})
+        result1 = [(node1[0], node2[0], edge_attrs1),
+                    (node2[0], node3[0], edge_attrs2)]
+        self.assertEqual(match_lst1, result1)
 
 
 
