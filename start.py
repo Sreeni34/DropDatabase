dde0808ca55759b1a6686ea00cfc5ed682f2722e
Ramruthwick Pathireddy <rpathire@caltech.edu>
2015-04-23 10:36:18 -0700

    Rewrote start.py to utilize a class abstraction.

--- start.py
+++ start.py
@@ -4,36 +4,59 @@ from linker import Linker
 from graph_structure import GraphStructure
 from graph_storage import GraphStorage
 import networkx as nx
+from utilities import Utilities
 
-# Start our main programs
-if __name__ == "__main__":
-    gs = GraphStructure()
-    gstorage = GraphStorage(gs)
-    print("Welcome to microDB!")
-    print("Loading database from disk...")
-    gstorage.load_file("TEST")
-    print("Enter commands into the interpreter below.")
-    not_done = True
-    while True:
-        commands = []
-        try:
+class StartDatabase:
+    """
+    Class runs the entire graph database in the terminal and manages
+    the entire interaction from user input to printing out data. 
+    """
+
+    def __init__(self):
+        """ 
+        Initializes the graph database by reading persisted data on disk
+        and setting up the internal in-memory graph file.
+        """        
+        print "Welcome to microDB!"
+        self.gs = GraphStructure()
+        self.gstorage = GraphStorage(self.gs)
+
+        # Loads data on disk
+        self.graph_file = 'graph_file'
+        self.id_file = 'id_file'
+        self.load_persistent_data()
+
+
+    def load_persistent_data(self):
+        """
+        Loads persisted data on disk, if it exists, into our GraphStructure
+        object.
+        """
+        print "Loading database from disk..."
+        if Utilities.files_exist(self.graph_file, self.id_file):
+            self.gstorage.load_graph(self.graph_file, self.id_file)
+            print "Finished loading database from disk."
+        else:
+            print "No files to load from."
+
+    def run(self):
+        """
+        Keeps the graph database and continously running in the terminal
+        and parses the input.
+        """
+        while True:
+            # Prints out the graph structure for testing purposes
+            self.gs.display()
+
+            commands = []
             sys.stdout.write(">>> ")
             while True:
-                try:
-                    command = raw_input()
-                    if (len(command) != 0 and command[-1] == ";"):
-                        commands.append(command)
-                        break
+                command = raw_input()
+                if (len(command) != 0 and command[-1] == ";"):
                     commands.append(command)
-                except KeyboardInterrupt:
-                    print("\nWriting database back to disk...")
-                    gstorage.write_file("TEST")
-                    print("Exiting microDB...")
-                    not_done = False
                     break
+                commands.append(command)
                 sys.stdout.write("... ")
-            if not not_done:
-                break
             command_str = " ".join(commands)
 
             # Start the parser and parse the commands
@@ -41,13 +64,32 @@ if __name__ == "__main__":
             parser.run()
 
             # Store the created objects in linker and call functions
-            linker = Linker(parser.get_object_list(), gs)
-            # linker.print_object_list()
-            # linker.link_object()
+            linker = Linker(parser.get_object_list(), self.gs)
             linker.execute()
 
-        except KeyboardInterrupt:
-            print("\nWriting database back to disk...")
-            gstorage.write_file("TEST")
-            print("Exiting microDB...")
-            break
+    def exit(self):
+        """
+        Persists the graph data and exits the graph database.
+        """
+        print
+        self.persist_data()
+        print "Exiting microDB..."
+
+    def persist_data(self):
+        """
+        Persists the GraphStructure object onto disk.
+        """
+        print "Writing database back to disk..." 
+        self.gstorage.write_graph(self.graph_file, self.id_file)
+
+
+# Start our main programs
+if __name__ == "__main__":
+    start = StartDatabase()
+    try:
+        start.run()
+    except KeyboardInterrupt:
+        start.exit()
+
+
+
