e36b130c86242f476efccde68501ae500955572e
Ramsathwick Pathireddy <rppathir@caltech.edu>
2015-04-25 00:05:28 -0700

    Added createEdge command, cleaned up parser code, and added comments.

--- Command_Struct.py
+++ Command_Struct.py
@@ -5,34 +5,136 @@ class Command_Struct:
     """
 
     def __init__(self, command):
+        """
+        Constructor that takes in name of command to create and
+        initializes the command, name and list of attributes. In 
+        the case of the command "createEdge", a list of dictionaries
+        would also be used.
+        """
         self.command = command
         self.name = ""
         self.attr = {}
 
+        # Variable to hold list of dictionary of attributes
+        # Specific to createEdge command
+        self.attr_list = []
+
+
     def set_name(self, name):
         """
         Set the current object name. 
+
+        @type name: String
+        @param name: String representing name of Command Struct object
         """
         self.name = name
+
 
     def insert_attr(self, attr1, attr2):
         """
         Insert the attribute to dictionary containing
         list of attributes.
+
+        @type attr1: String
+        @param attr1: The key to add to dictionary
+        @type attr2: String 
+        @param attr2: The value to add to dictionary
         """
         self.attr[attr1] = attr2
 
+
+    def insert_attr_list(self, ind):
+        """
+        Method adds the current attribute dictionary to list of 
+        dictionaries at specified index location.
+
+        @type ind: Integer
+        @param ind: Index location to insert the dictionary into list
+        """
+        if (ind >= len(self.attr_list)):
+            self.attr_list.append(self.attr)
+        else:
+            self.attr_list[ind] = self.attr
+
+
+    def clear_attr(self):
+        """
+        Clears the attribute list.
+        """
+        self.attr = {}
+
+
     def get_command(self):
-        return command
+        """
+        Returns the command of this object.
+
+        @rtype: String
+        @return: String representing command of object
+        """
+        return self.command
+
 
     def get_name(self):
+        """
+        Returns the name of the object if the object has one.
+        "NO NAME" is supplied if none exits.
+
+        @rtype: String 
+        @return: String representing the name of object
+        """
         return self.name
 
+
     def get_attr(self):
-        return self.attr;
+        """
+        Returns the list of attributes for the command. Method 
+        works for commands CREATE, MATCH, and RETURN.
+
+        @rtype: dictionary
+        @return: List of attributes in dictionary format
+        """
+        return self.attr
+
+
+    def get_attr_list(self):
+        """
+        Returns the list of dictionaries that represents attributes.
+        Method works for command CREATEEDGE.
+
+        @rtype: List of dictionaries
+        @return: List of dictionaries representing attributes for 
+        createEdge command.
+        """
+        return self.attr_list
+
+
+    def get_attr_size(self):
+        """
+        Returns the size of the attribute dictionary.
+
+        @rtype: Integer
+        @return: Integer representing size of dictionary.
+        """
+        return len(self.attr)
+
+
+    def get_attr_list_size(self):
+        """
+        Returns the number of dictionaries in list.
+
+        @rtype: Integer
+        @return: Number of dictionaries in list.
+        """
+        return len(self.attr_list)
+
 
     def print_Class(self):
         print "Command: " + self.command
         print "Name: " + self.name
-        print "Attributes ",
+        print "Attributes: ",
         print self.attr
+        
+        if (self.command == "CREATEEDGE"):
+            print "List of Attributes: ",
+            print self.attr_list
+
--- parser.py
+++ parser.py
@@ -8,16 +8,19 @@ from Command_Struct import Command_Struct
 
 # Global token and state definitions. 
 
-NUM_TOKENS = 7
-NUM_STATES = 7 # number of states with actual transitions (excludes error state)
+NUM_TOKENS = 10
+NUM_STATES = 8 # number of states with actual transitions (excludes error state)
 
 TOKEN_CREATE = 0
 TOKEN_MATCH = 1
 TOKEN_RETURN = 2
 TOKEN_NAME = 3
 TOKEN_ATTR = 4
-TOKEN_ERROR = 5
-TOKEN_END = 6
+TOKEN_EDGE = 5
+TOKEN_NODE = 6
+TOKEN_REL = 7
+TOKEN_ERROR = 8
+TOKEN_END = 9
 
 STATE_INIT = 0
 STATE_CREATE = 1
@@ -25,8 +28,9 @@ STATE_MATCH = 2
 STATE_RETURN = 3
 STATE_NAME = 4
 STATE_ATTR = 5
-STATE_END = 6
-STATE_ERROR = 7
+STATE_EDGE = 6
+STATE_END = 7
+STATE_ERROR = 8
 
 
 class Parser:
@@ -36,6 +40,7 @@ class Parser:
         self.parseStr = parsedString
         self.words = parsedString.split()
         self.current_token = -1  # current token being processed by the parser
+        self.curr_list_token = -1 # specific for createEdge command
         self.current_state = STATE_INIT  # current state 
         self.current_word = ''
         self.obj_list = []       #Store a list of command objects
@@ -57,6 +62,9 @@ class Parser:
         state_machine[STATE_INIT][TOKEN_RETURN] = (STATE_RETURN, self.no_op)
         state_machine[STATE_INIT][TOKEN_NAME] = (STATE_ERROR, self.error)
         state_machine[STATE_INIT][TOKEN_ATTR] = (STATE_ERROR, self.error)
+        state_machine[STATE_INIT][TOKEN_EDGE] = (STATE_EDGE, self.add_createEdge_object)
+        state_machine[STATE_INIT][TOKEN_NODE] = (STATE_ERROR, self.error)
+        state_machine[STATE_INIT][TOKEN_REL] = (STATE_ERROR, self.error)
         state_machine[STATE_INIT][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_INIT][TOKEN_END] = (STATE_END, self.finish)
 
@@ -66,6 +74,9 @@ class Parser:
         state_machine[STATE_CREATE][TOKEN_RETURN] = (STATE_ERROR, self.error)
         state_machine[STATE_CREATE][TOKEN_NAME] = (STATE_NAME, self.add_create_object)
         state_machine[STATE_CREATE][TOKEN_ATTR] = (STATE_ERROR, self.error)
+        state_machine[STATE_CREATE][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_CREATE][TOKEN_NODE] = (STATE_ERROR, self.error)
+        state_machine[STATE_CREATE][TOKEN_REL] = (STATE_ERROR, self.error)
         state_machine[STATE_CREATE][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_CREATE][TOKEN_END] = (STATE_ERROR, self.error)
 
@@ -75,6 +86,9 @@ class Parser:
         state_machine[STATE_MATCH][TOKEN_RETURN] = (STATE_ERROR, self.error)
         state_machine[STATE_MATCH][TOKEN_NAME] = (STATE_NAME, self.add_match_object)
         state_machine[STATE_MATCH][TOKEN_ATTR] = (STATE_ERROR, self.error)
+        state_machine[STATE_MATCH][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_MATCH][TOKEN_NODE] = (STATE_ERROR, self.error)
+        state_machine[STATE_MATCH][TOKEN_REL] = (STATE_ERROR, self.error)
         state_machine[STATE_MATCH][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_MATCH][TOKEN_END] = (STATE_ERROR, self.error)
 
@@ -84,6 +98,9 @@ class Parser:
         state_machine[STATE_RETURN][TOKEN_RETURN] = (STATE_ERROR, self.error)
         state_machine[STATE_RETURN][TOKEN_NAME] = (STATE_END, self.add_return_object)
         state_machine[STATE_RETURN][TOKEN_ATTR] = (STATE_ERROR, self.error)
+        state_machine[STATE_RETURN][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_RETURN][TOKEN_NODE] = (STATE_ERROR, self.error)
+        state_machine[STATE_RETURN][TOKEN_REL] = (STATE_ERROR, self.error)
         state_machine[STATE_RETURN][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_RETURN][TOKEN_END] = (STATE_ERROR, self.error)
 
@@ -93,6 +110,9 @@ class Parser:
         state_machine[STATE_NAME][TOKEN_RETURN] = (STATE_ERROR, self.error)
         state_machine[STATE_NAME][TOKEN_NAME] = (STATE_ERROR, self.error)
         state_machine[STATE_NAME][TOKEN_ATTR] = (STATE_ATTR, self.add_attr)
+        state_machine[STATE_NAME][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_NAME][TOKEN_NODE] = (STATE_ERROR, self.error)
+        state_machine[STATE_NAME][TOKEN_REL] = (STATE_ERROR, self.error)
         state_machine[STATE_NAME][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_NAME][TOKEN_END] = (STATE_ERROR, self.error)
 
@@ -102,8 +122,23 @@ class Parser:
         state_machine[STATE_ATTR][TOKEN_RETURN] = (STATE_RETURN, self.no_op)
         state_machine[STATE_ATTR][TOKEN_NAME] = (STATE_ERROR, self.error)
         state_machine[STATE_ATTR][TOKEN_ATTR] = (STATE_ATTR, self.add_attr)
+        state_machine[STATE_ATTR][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_ATTR][TOKEN_NODE] = (STATE_ATTR, self.add_list_attr)
+        state_machine[STATE_ATTR][TOKEN_REL] = (STATE_ATTR, self.add_list_attr)
         state_machine[STATE_ATTR][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_ATTR][TOKEN_END] = (STATE_END, self.finish)
+
+        # STATE_EDGE
+        state_machine[STATE_EDGE][TOKEN_CREATE] = (STATE_ERROR, self.no_op)
+        state_machine[STATE_EDGE][TOKEN_MATCH] = (STATE_ERROR, self.no_op)
+        state_machine[STATE_EDGE][TOKEN_RETURN] = (STATE_ERROR, self.no_op)
+        state_machine[STATE_EDGE][TOKEN_NAME] = (STATE_ERROR, self.error)
+        state_machine[STATE_EDGE][TOKEN_ATTR] = (STATE_ERROR, self.error)
+        state_machine[STATE_EDGE][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_EDGE][TOKEN_NODE] = (STATE_ATTR, self.add_list_attr)
+        state_machine[STATE_EDGE][TOKEN_REL] = (STATE_ERROR, self.error)
+        state_machine[STATE_EDGE][TOKEN_ERROR] = (STATE_ERROR, self.error)
+        state_machine[STATE_EDGE][TOKEN_END] = (STATE_END, self.finish)
 
         # STATE_END
         state_machine[STATE_END][TOKEN_CREATE] = (STATE_ERROR, self.error)
@@ -111,6 +146,9 @@ class Parser:
         state_machine[STATE_END][TOKEN_RETURN] = (STATE_ERROR, self.error)
         state_machine[STATE_END][TOKEN_NAME] = (STATE_ERROR, self.error)
         state_machine[STATE_END][TOKEN_ATTR] = (STATE_ERROR, self.error)
+        state_machine[STATE_END][TOKEN_EDGE] = (STATE_ERROR, self.error)
+        state_machine[STATE_END][TOKEN_NODE] = (STATE_ERROR, self.error)
+        state_machine[STATE_END][TOKEN_REL] = (STATE_ERROR, self.error)
         state_machine[STATE_END][TOKEN_ERROR] = (STATE_ERROR, self.error)
         state_machine[STATE_END][TOKEN_END] = (STATE_END, self.finish)
 
@@ -118,7 +156,15 @@ class Parser:
 
 
     def get_token(self, word):
+        """
+        This method takes in a word entered by the user and 
+        returns token specifying the next execuation step. 
 
+        @type word: string
+        @param word: The word to convert to token
+        @rtype: Integer
+        @return: An integer which represents a specific token command
+        """
 
         if (word.lower() == "create"):
             return TOKEN_CREATE
@@ -126,6 +172,12 @@ class Parser:
             return TOKEN_MATCH
         elif (word.lower() == "return"):
             return TOKEN_RETURN
+        elif (word.lower() == "createedge"):
+            return TOKEN_EDGE
+        elif (word.lower() == "n:"):
+            return TOKEN_NODE
+        elif (word.lower() == "e:"):
+            return TOKEN_REL
         elif (word == ";"):
             return TOKEN_END
         elif (word.count(":") == 1):
@@ -134,16 +186,6 @@ class Parser:
             return TOKEN_NAME
         else:
             return TOKEN_ERROR
-
-    def get_object_list(self):
-        return self.obj_list
-
-    def print_object_list(self):
-        print self.obj_list
-        for obj in self.obj_list:
-            obj.print_Class()
-
-
 
     # Callback methods used in the finite state machine.
 
@@ -195,13 +237,113 @@ class Parser:
         self.obj_list.append(self.curr_obj)
 
 
+    def add_createEdge_object(self):
+        """ 
+        Create a Command_Struct object that represents a createEdge
+        command entered by the user. Append the object to list
+        of command objects. Also have a token state to help us keep
+        track of the arguments supplied to command.
+        """
+        self.curr_obj = Command_Struct("CREATEEDGE")
+        self.curr_obj.set_name("NO NAME")
+        self.obj_list.append(self.curr_obj)
+
+        # Reset token object to mention that we are in this command
+        self.curr_list_token = 0;
+
+
     def add_attr(self):
         """
         Add argument key-value pair to internal attributes dictionary 
-        for specific command object.
+        for specific command object. Take care of the special case
+        where the command is CREATEEDGE.
         """
-        lst = self.current_word.split(":")
-        self.curr_obj.insert_attr(lst[0], lst[1])
+        if (self.curr_obj.get_command() == "CREATEEDGE"):
+            self.add_list_attr()
+        else:
+            lst = self.current_word.split(":")
+            self.curr_obj.insert_attr(lst[0], lst[1])
+
+
+    def add_list_attr(self):
+        """
+        Helper method for the add_attr method. This takes care of
+        the special case when CreateEdge is supplied as an argument.
+        The method inserts the attribute into the appropriate location
+        in our list of attributes dictionary. 
+        """
+
+        # Error because we are not in the createEdge command
+        if (self.curr_list_token == -1):
+            self.error()
+
+        # In createEdge command but have not initialized state
+        # The first state would be the n: state
+        elif (self.curr_list_token == 0):
+            self.curr_list_token = TOKEN_NODE
+
+        # Current state is either n: or e: but we are simply
+        # adding an attribute to our dictionary
+        elif (self.current_token == TOKEN_ATTR):
+            # Add to dictionary
+            lst = self.current_word.split(":")
+            self.curr_obj.insert_attr(lst[0], lst[1])
+
+            # Update list of dictionary object
+            self.update_list_dict()
+
+        # Transition from attr in n: to e:
+        elif (self.curr_list_token == TOKEN_NODE and self.current_token == TOKEN_REL):
+            # Update current list token and clear list
+            self.update_token_and_clear(TOKEN_REL, 1)
+
+        # Transition from attr in e: to n:
+        elif (self.curr_list_token == TOKEN_REL and self.current_token == TOKEN_NODE):
+            # Update current list token and clear list
+            self.update_token_and_clear(TOKEN_NODE, 2)
+
+        # No-valid state
+        else:
+            self.error_Message("add_list_attr");
+
+
+    def update_list_dict(self):
+        """
+        This is a helper function to the add_list_attr method
+        which updates the list of dictionaries in the Command_Struct
+        object. 
+        """
+        # Update first index in list of dict object
+        if (self.curr_list_token == TOKEN_NODE and
+            (self.curr_obj.get_attr_list_size() == 0 or
+            self.curr_obj.get_attr_list_size() == 1)):
+
+            self.curr_obj.insert_attr_list(0)
+        # Update second index in list of dict object
+        elif (self.curr_list_token == TOKEN_REL):
+            self.curr_obj.insert_attr_list(1)
+        # Update third index in list of dict object
+        elif (self.curr_list_token == TOKEN_NODE):
+            self.curr_obj.insert_attr_list(2)
+        # Invalid Token Read
+        else:
+            self.error_Message("update_list_dict");
+
+
+    def update_token_and_clear(self, token, size):
+        """
+        This method resets the token to represent the new 
+        state (n: or e:) we are in and clears the previous
+        list of attributes.
+        """
+        # Check if the sizes are satisfied
+        if (self.curr_obj.get_attr_list_size() == size):
+            # Update pointer, and clear attributes
+            self.curr_list_token = token
+            self.curr_obj.clear_attr()
+        # Sizes do not match, problem with add_list_attr
+        else:
+            self.error_Message("update_token_and_clear");
 
 
 ############################################################
@@ -209,15 +351,49 @@ class Parser:
 ############################################################
     
     def error(self):
+        """
+        Prints out an error and exits the program.
+        """
         print "TODO : ERROR MESSAGE HERE"
         self.done = True
+
+
+    def error_Message(self, message):
+        """
+        Prints out an error message specifying the method
+        where the error had occurred. Program will stop
+        immediately. 
+
+        @type message: string
+        @param message: Error message to print our to user
+        """
+
+        print "ERROR: " + message
+        self.done = True
+
+
     def finish(self):
         """ 
         Indicate the end of parsing. 
         """
         self.done = True
 
+    def get_object_list(self):
+        """
+        Return the list of objects representing commands specified
+        by user.
+
+        @rtype: List of Command_Struct objects
+        @return: Object list representing commands and arguments
+        """
+        return self.obj_list
+
+
     def run(self):
+        """
+        Runs the parser and creates an object list containing 
+        information about commands and their arguments.
+        """
         
         for word in self.words:
             # print "this is the word: " + word
