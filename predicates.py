4a59ae956e978cf9def8073b67092925e44623fc
amandlek <ajay.mandlekar@gmail.com>
2015-04-30 01:36:56 -0700

    Completed a basic implementation of predicate handling in predicates.py, but I haven't tested it yet. Will need to do testing later to verify correct operation.

--- predicates.py
+++ predicates.py
@@ -13,5 +13,134 @@ class Predicates:
         self.gs = gs
         self.g = gs.get_graph()   
 
-    def less_than(self, key, value):
+    def str2float(self, value):
+        """
+        Helper function that tries to convert a string representing a number
+        to a floating point number. If the string does not represent a 
+        number, returns the string "ERROR". 
+
+        @type val: String that represents a number.
+        @param val: String to convert to a float. 
+        @rtype: Float
+        @return: The converted string as a float, or 
+                 "ERROR" if an error occurred. 
+        """
+        try:
+            float_val = float(value)
+            return float_val 
+        except ValueError:
+            return "ERROR"
+
+    def filter(self, node_list, attr, value, op):
+        """
+        Function that takes a list of node ids, an
+        attribute to filter on, a value to filter on, and an
+        operation string. Returns a filtered list of node ids that 
+        satisfy the predicate consisting of the attribute, 
+        value and operation. 
+
+        @type node_list: List
+        @param node_list: List of node ids
+        @type attr: String
+        @param attr: Attribute used to filter node
+        @type value: String that represents a number.
+        @param value: Value to compare the node attributes against. 
+        @type op: String that is either "<", "=", or ">".
+        @param op: Operation to perform when comparing each node attribute 
+                   against value. 
+        @rtype: List
+        @return: Filtered list of nodes that satisfy the predicate. 
+        """
+        val = str2float(value)
+        if val == "ERROR":
+            print "ERROR : Comparison Value must be an number..."
+            return []
+        if op == "<":
+            return self.filter_less(node_list, attr, val)
+        elif op == ">":
+            return self.filter_greater(node_list, attr, val)
+        elif op == "=":
+            return self.filter_equal(node_list, attr, val)
+        else:
+            print "ERROR : Invalid predicate operation..."
+            return []
+
+    def filter_less(self, node_list, attr, val):
+        """
+        Function that takes a list of node ids, an
+        attribute to filter on, and a value to filter on. 
+        Returns a filtered list of node ids whose attributes 
+        are less than the passed value. 
+
+        @type node_list: List
+        @param node_list: List of node ids
+        @type attr: String
+        @param attr: Attribute used to filter node
+        @type value: Float
+        @param value: Value to compare the node attributes against. 
+        @rtype: List
+        @return: Filtered list of nodes that satisfy the predicate. 
+        """
+        ret = []
+        for n in node_list:
+            attr_val = str2float(self.g[n][attr])
+            if attr_val == "ERROR":
+                print "ERROR : Got attribute value that is not a number..."
+                continue
+            if attr_val < val:
+                ret.append(n)
+        return ret 
+
+    def filter_greater(self, node_list, attr, val):
+        """
+        Function that takes a list of node ids, an
+        attribute to filter on, and a value to filter on. 
+        Returns a filtered list of node ids whose attributes 
+        are greater than the passed value. 
+
+        @type node_list: List
+        @param node_list: List of node ids
+        @type attr: String
+        @param attr: Attribute used to filter node
+        @type value: Float
+        @param value: Value to compare the node attributes against. 
+        @rtype: List
+        @return: Filtered list of nodes that satisfy the predicate. 
+        """
+        ret = []
+        for n in node_list:
+            attr_val = str2float(self.g[n][attr])
+            if attr_val == "ERROR":
+                print "ERROR : Got attribute value that is not a number..."
+                continue
+            if attr_val > val:
+                ret.append(n)
+        return ret 
+
+    def filter_equal(self, node_list, attr, val):
+        """
+        Function that takes a list of node ids, an
+        attribute to filter on, and a value to filter on. 
+        Returns a filtered list of node ids whose attributes 
+        are equal to the passed value. 
+
+        @type node_list: List
+        @param node_list: List of node ids
+        @type attr: String
+        @param attr: Attribute used to filter node
+        @type value: Float
+        @param value: Value to compare the node attributes against. 
+        @rtype: List
+        @return: Filtered list of nodes that satisfy the predicate. 
+        """
+        ret = []
+        for n in node_list:
+            attr_val = str2float(self.g[n][attr])
+            if attr_val == "ERROR":
+                print "ERROR : Got attribute value that is not a number..."
+                continue
+            if attr_val == val:
+                ret.append(n)
+        return ret 
+
 
